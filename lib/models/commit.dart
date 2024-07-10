e785f98a240a415f7903eb73eb6f017141a7928c
jidnjs <jkang.2005@icloud.com>
2024-07-10 17:55:28 +0900

    aaaaa

--- lib/models/commit.dart
+++ lib/models/commit.dart
@@ -23,14 +23,16 @@ class Commit {
 
   factory Commit.fromJson(Map<String, dynamic> json) {
     return Commit(
-      commitId: json['commit_id'],
-      aiLabeled: json['ai_labeled'],
+      commitId: json['commit_id'] is String ? int.parse(json['commit_id']) : json['commit_id'],
+      aiLabeled: json['ai_labeled'] is String ? int.parse(json['ai_labeled']) : json['ai_labeled'],
       commitMsg: json['commit_msg'],
       commitDate: DateTime.parse(json['commit_date']),
-      tilId: json['til_id'],
-      issueIdList: json['issue_id_list'],
-      ownerGithubId: json['owner_github_id'],
-      repoGithubId: json['repo_github_id'],
+      tilId: json['til_id'] is String ? int.parse(json['til_id']) : json['til_id'],
+      issueIdList: json['issue_id_list'] is String
+          ? json['issue_id_list'].split(',').map((e) => e.trim()).toList()
+          : List<dynamic>.from(json['issue_id_list']),
+      ownerGithubId: json['owner_github_id'] is String ? int.parse(json['owner_github_id']) : json['owner_github_id'],
+      repoGithubId: json['repo_github_id'] is String ? int.parse(json['repo_github_id']) : json['repo_github_id'],
       commitGithubId: json['commit_github_id'],
     );
   }
